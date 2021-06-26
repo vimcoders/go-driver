0580f6d082d862e1012c2197e48d8bed5422b8c1
vimcoders <vimcoders@gmail.com>
2021-06-26 21:30:58 +0800

    connector

--- driver.go
+++ driver.go
@@ -1,5 +1,9 @@
 package driver
 
+import (
+	"context"
+)
+
 type Logger interface {
 	Debug(format string, v ...interface{})
 	Info(format string, v ...interface{})
@@ -18,4 +22,43 @@ type Session interface {
 	Get(key interface{}) interface{}
 	Delete(key interface{}) error
 	Write(pkg Message) (err error)
+}
+
+type Connector interface {
+	Tx(ctx context.Context) (Tx, error)
+	Conn(ctx context.Context) (Conn, error)
+	SetMaxOpenConns(n int)
+	Close() error
+}
+
+type Tx interface {
+	Exec(obj Object) (interface{}, error)
+	ExecContext(ctx context.Context, obj Object) (interface{}, error)
+	Rollback() (err error)
+	Commit() (err error)
+}
+
+type Conn interface {
+	Exec(obj Object) (interface{}, error)
+	ExecContext(ctx context.Context, obj Object) (interface{}, error)
+	Tx(ctx context.Context) (Tx, error)
+	Close() (err error)
+}
+
+type Object interface {
+	Table
+	Convert() (sql string, args []interface{})
+	Scan(scanner func(dest ...interface{}) error) error
+}
+
+type Table interface {
+	TableName() string
+}
+
+type Marshaler interface {
+	Marshal(str string, err error)
+}
+
+type Unmarshaler interface {
+	Unmarshal(str string) error
 }
