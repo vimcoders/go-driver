3e16cd02f4efe2c07aa43ae62e2f06e4394ad0bc
Jim <vimcoders@gmail.com>
2021-07-03 10:19:03 +0800

    driver

--- driver.go
+++ driver.go
@@ -31,28 +31,9 @@ type Connector interface {
 	Close() error
 }
 
-type Scanner interface {
-	Scan(scanner func(dest ...interface{}) error) error
-}
-
-type Convertor interface {
-	Convert() (sql string, args []interface{})
-}
-
-type Table interface {
-	TableName() string
-}
-
-type Marshaler interface {
-	Marshal() (str string, err error)
-}
-
-type Unmarshaler interface {
-	Unmarshal(str string) error
-}
-
 type Result sql.Result
 
 type Execer interface {
-	Exec(c ...Convertor) (Result, error)
+	Exec(i ...interface{}) (Result, error)
+	ExecContext(ctx context.Context, i ...interface{}) (Result, error)
 }
