ccf1ae5b50c853d87c896218110f7dd56579c8d3
Jim <vimcoders@gmail.com>
2022-08-15 09:29:28 +0000

    tx

--- driver.go
+++ driver.go
@@ -13,12 +13,36 @@ type Logger interface {
 }
 
 type Connector interface {
-	Tx(ctx context.Context) (Execer, error)
+	Tx(ctx context.Context) (Tx, error)
 	SetMaxOpenConns(n int)
 	Close() error
 }
 
-type Execer interface {
-	Exec(i ...interface{}) (interface{}, error)
-	ExecContext(ctx context.Context, i ...interface{}) (interface{}, error)
+type Table interface {
+	TableName() string
+}
+
+type Updater interface {
+	Update(i interface{}) (interface{}, error)
+}
+
+type Deleter interface {
+	Delete(i interface{}) (interface{}, error)
+}
+
+type Inserter interface {
+	Insert(i interface{}) (interface{}, error)
+}
+
+type Queryer interface {
+	Query(i interface{}) ([]interface{}, error)
+}
+
+type Tx interface {
+	Updater
+	Deleter
+	Inserter
+	Queryer
+	Commit() error
+	Rollback() error
 }
