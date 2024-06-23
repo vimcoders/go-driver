9f7c1ff68615a3e6889de6aa2c2c42230a402efc
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-06-23 12:54:37 +0800

    driver

--- tcp/tcp_test.go
+++ tcp/tcp_test.go
@@ -18,29 +18,10 @@ import (
 	"go-driver/tcp"
 )
 
-type Buffer []byte
+type Buffer = driver.Buffer
 
 func NewBuffer(size int) Buffer {
-	return make([]byte, size)
-}
-
-func (b *Buffer) Reset() {
-	*b = (*b)[:0]
-}
-
-func (b *Buffer) Write(p []byte) (int, error) {
-	*b = append(*b, p...)
-	return len(p), nil
-}
-
-func (b *Buffer) WriteString(s string) (int, error) {
-	*b = append(*b, s...)
-	return len(s), nil
-}
-
-func (b *Buffer) WriteByte(c byte) error {
-	*b = append(*b, c)
-	return nil
+	return driver.NewBuffer(size)
 }
 
 type TcpSession struct {
@@ -130,7 +111,7 @@ func (x *Handler) Close() error {
 }
 
 type Session struct {
-	total          int64
+	//total          int64
 	ReaderBuffsize int
 	Header         int
 	Timeout        time.Duration
