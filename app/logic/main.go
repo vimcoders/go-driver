b6544231b6d23bba5fa183a6aa48ef713fadb36f
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-06-30 14:42:25 +0800

    rpcx

--- app/logic/main.go
+++ app/logic/main.go
@@ -13,6 +13,7 @@ import (
 	"math/big"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 	"time"
 
@@ -28,6 +29,7 @@ import (
 )
 
 func main() {
+	runtime.GOMAXPROCS(1)
 	log.Info("RUNNING")
 	var fileName string
 	flag.StringVar(&fileName, "conf", "./logic.conf", "logic.conf")
