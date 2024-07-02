306016fdf99aeb141708fce4c6148555e108b38d
DESKTOP-2G5B5KH\xunfeng-lujianbo <lujianbo@xunfeng.cn>
2024-07-02 14:12:41 +0800

    ServeTCP

--- app/balance/handle/session.go
+++ app/balance/handle/session.go
@@ -2,16 +2,15 @@ package handle
 
 import (
 	"context"
+	"errors"
 	"go-driver/grpcx"
-	"go-driver/pb"
 	"go-driver/tcp"
+	"reflect"
 	"strings"
 	"time"
 
 	"google.golang.org/protobuf/proto"
 )
-
-var HandlerDesc = pb.Handler_ServiceDesc
 
 type Session struct {
 	tcpclient tcp.Client
@@ -24,29 +23,12 @@ func (x *Session) ServeTCP(ctx context.Context, request proto.Message) error {
 	defer cancel()
 	messageName := string(proto.MessageName(request).Name())
 	methodName := strings.TrimSuffix(messageName, "Request")
-	dec := func(in any) error {
-		b, err := proto.Marshal(request)
-		if err != nil {
-			return err
-		}
-		if err := proto.Unmarshal(b, in.(proto.Message)); err != nil {
-			return err
-		}
-		return nil
+	method := reflect.ValueOf(x.rpcclient).MethodByName(methodName)
+	result := method.Call([]reflect.Value{reflect.ValueOf(timeoutCtx), reflect.ValueOf(request)})
+	if len(result) <= 0 {
+		return errors.New("len(result) <= 0")
 	}
-	for i := 0; i < len(HandlerDesc.Methods); i++ {
-		if HandlerDesc.Methods[i].MethodName != methodName {
-			continue
-		}
-		response, err := HandlerDesc.Methods[i].Handler(x.rpcclient, timeoutCtx, dec, nil)
-		if err != nil {
-			return err
-		}
-		if err := x.tcpclient.Go(ctx, response.(proto.Message)); err != nil {
-			return err
-		}
-		return nil
-	}
+	x.tcpclient.Go(ctx, result[0].Interface().(proto.Message))
 	return nil
 }
 
