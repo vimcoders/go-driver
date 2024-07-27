60f42370f2f8f09efad7814d624cc67cca69dd08
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-07-28 03:45:22 +0800

    xlsx

--- dingding/dingding_test.go
+++
@@ -1,7 +0,0 @@
-package dingding
-
-import "testing"
-
-func TestVerify(t *testing.T) {
-	//https://oapi.dingtalk.com/robot/send?access_token=d73d82bb4046ff1cdecb1630b068ce13da18337d031318a9ea2d0c5fc88bcf4f
-}
